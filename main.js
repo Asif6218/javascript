5e32ed8ff36ad650acfa0f690aa808110b741d6c
Asif6218 <mohdasif6227@gmail.com>
2021-07-24 20:25:35 -0600

    javascript

---
+++ main.js
@@ -0,0 +1,32 @@
+//datatypes
+//1.Primitive datatype
+//2.Refferential datatype
+
+//PRIMITIVE DATATYPE
+//1.Undefined
+//2.String
+//3.Number
+//4.Symbol
+//5.Boolean
+//6.Null
+
+var a=14;
+var b="fujifilm";
+var c=true;
+var d=null;
+var e;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
+
+console.log(f);
+var f=100;
